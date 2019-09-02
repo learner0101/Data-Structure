b0b41af1e4d79855724cf05f5eda7931956c970d
learner <31645832+learner0101@users.noreply.github.com>
2019-09-02 17:37:18 +0530

    Add files via upload
    Change an array into its reduced form.
    array input-> 10,5,20,9
    output-> 2,0,3,1

---
+++ Array/array reduced form.cpp
@@ -0,0 +1,56 @@
+//C++ program to convert an array into its reduced form
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void convert(int a[], int n)
+{
+    //create temp array and copy all elements into it.
+    int temp[n];
+    memcpy(temp,a,n*sizeof(int));
+    
+    //sort array
+    sort(temp,temp+n);
+    
+    //create hash table
+    unordered_map<int , int> umap;
+    int val=0;
+    for(int i=0;i<n;i++)
+    {
+        umap[temp[i]]=val++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        a[i]=umap[a[i]];
+    }
+    
+}
+//print array
+void printArray(int a[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+
+//Driver program 
+int main() 
+{
+	//code
+	int T,N;
+	cin>>T; //Test case
+	while(T)
+	{
+	    cin>>N; //Array size
+	    int a[N];
+	    for(int i=0;i<N;i++)
+	        cin>>a[i];
+	   convert(a,N);
+	   printArray(a,N);
+	   T--;
+	   cout<<endl;
+	}
+
+	return 0;
+}
